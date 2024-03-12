23d5d8fb6d4cc7d4a67a558881b6ae8567cf51e5
vcheethi <95873332+vcheethi@users.noreply.github.com>
2024-03-12 16:20:24 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,39 @@
-variable "log_analytics_workspace_name" {
-    description = ""
-    type        = string
-}
 variable "location" {
-    description = ""
-    type        = string
+  description = "Name of the location where the resources will be provisioned"
+  type = string
 }
 
 variable "resource_group_name" {
-    description = ""
+  description = "Name of the resource group"
+  type = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type = string
+}
+
+
+variable "service_plan_id" {
     type        = string
 }
 
-variable "application_insights_name" {
-    description = ""
+
+
+variable "function_name" {
+  description = "Name of the function"
+  type = string
+}
+
+variable "app_settings" {
+    #
+}
+
+variable "vnet_integration_required" {
+    type        = bool
+}
+
+variable "subnet_id_for_vnet_integration" {
     type        = string
 }
 
@@ -23,10 +43,4 @@ variable "tags" {
   default     = {}
 }
 
-variable "sku" {
-    default   = "PerGB2018"
-}
 
-variable "retention_in_days" {
-    default   = 30
-}
